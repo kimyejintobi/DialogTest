6c6f644b653a156b9162c2c62dd22298db86bf7f
kimyejin <kimyejintobi@gmail.com>
2017-05-25 20:49:40 +0900

    #2 setPositiveButton

--- app/src/main/java/kr/hs/emirim/petto99/dialogtest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/petto99/dialogtest/MainActivity.java
@@ -1,20 +1,37 @@
 package kr.hs.emirim.petto99.dialogtest;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+    String[] itemArr={"Ryujehong","Zunba","tobi","EscA","WhoRu","Gido","Miro"};
+    Button butDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button butDialog=(Button) findViewById(R.id.but_dialog);
+        butDialog=(Button) findViewById(R.id.but_dialog);
         butDialog.setOnClickListener(this);
     }
     @Override
-    public void onClick(View view){}
+    public void onClick(View view){
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setTitle("Lunatic-Hai");
+        dialog.setIcon(R.mipmap.tobi); //mipmap은 사진을 아이콘을 만들어준다.
+       // dialog.setMessage("이 곳에 메세지를 입력하세요!");
+        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                butDialog.setText(itemArr[i]);
+
+            }
+        });
+        dialog.setPositiveButton("OK",null);
+        dialog.show();
+    }
 
 }
